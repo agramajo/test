dfd0a37f7b8131b6467f4dc9cfbc93abed5df784
pgotcha <pablo.olivera@wegotcha.io>
2022-05-31 17:51:28 -0300

    feat: new date 2

--- packages/beta/index.js
+++ packages/beta/index.js
@@ -1,5 +1,6 @@
 export default function getBeta () {
-  console.log(`Actual new Date: ${new Date()}`);
+  console.log(`Actual new Date 1: ${new Date()}`);
+  console.log(`Actual new Date 2: ${new Date()}`);
   return new Date();
 };
 
